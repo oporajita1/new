7658724d4dff957c918ac07c4cdfe2819e2c10e4
oporajita1 <rough82@yahoo.com>
2016-12-08 18:50:17 +0600

    .cpp

---
+++ iterator.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include<vector>
+#include <cstdio>
+using namespace std;
+int main()
+{
+
+vector< int > v;
+v.push_back( 1 ); v.push_back( 2 ); v.push_back( 3 );
+vector< int > :: iterator i;
+for( i = v.begin(); i < v.end(); i++ ) {
+        printf("%d\n", *i);
+        // ei khane gola kato!
+    }
+}
